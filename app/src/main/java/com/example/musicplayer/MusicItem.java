c6422950f21d97ffd11691ddbd26d10a2d032211
LiuchangDuan <870121425@qq.com>
2016-11-06 17:09:14 +0800

    获取音乐信息

---
+++ app/src/main/java/com/example/musicplayer/MusicItem.java
@@ -0,0 +1,40 @@
+package com.example.musicplayer;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+/**
+ * Created by Administrator on 2016/11/6.
+ *
+ * 1.定义一个音乐信息的数据结构MusicItem
+ */
+public class MusicItem {
+
+    // 存储音乐的名字
+    String name;
+    //存储音乐的Uri地址
+    Uri songUri;
+    // 存储音乐封面的Uri地址
+    Uri albumUri;
+    // 存储封面图片
+    Bitmap thumb;
+    // 存储音乐的播放时长，单位是毫秒
+    long duration;
+    long playedTime;
+
+    public MusicItem(Uri songUri, Uri albumUri, String strName, long duration, long playedTime) {
+        this.name = strName;
+        this.songUri = songUri;
+        this.albumUri = albumUri;
+        this.duration = duration;
+        this.playedTime = playedTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        MusicItem another = (MusicItem) o;
+
+        return another.songUri.equals(this.songUri);
+    }
+
+}
--- app/src/main/java/com/example/musicplayer/MusicListActivity.java
+++ app/src/main/java/com/example/musicplayer/MusicListActivity.java
@@ -1,13 +1,91 @@
 package com.example.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MusicListActivity extends AppCompatActivity {
+
+    public static final String TAG = "MusicListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_list);
     }
+
+    private class MusicUpdateTask extends AsyncTask<Object, MusicItem, Void> {
+        List<MusicItem> mDataList = new ArrayList<MusicItem>();
+
+        @Override
+        protected Void doInBackground(Object... params) {
+            /**
+             * 向Media Provider发出查询请求的地址 - uri
+             * 这里查询的音乐文件都是存放在外部存储地址上的
+             */
+            Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+            String[] searchKey = new String[] {
+                MediaStore.Audio.Media._ID, // 对应文件在数据库中的检索ID
+                MediaStore.Audio.Media.TITLE, // 对应文件的标题
+                MediaStore.Audio.Albums.ALBUM_ID, // 对应文件所在的专辑ID，在后面获取封面图片时会用到
+                MediaStore.Audio.Media.DATA, // 对应文件的存放位置
+                MediaStore.Audio.Media.DURATION // 对应文件的播放时长
+            };
+
+            // 查询包含有music这个字段的文件
+            String where = MediaStore.Audio.Media.DATA + " like \"%" + getString(R.string.searh_path) + "%\"";
+
+            String[] keywords = null;
+
+            // 设定查询结果的排序方式，使用默认的排序方式
+            String sortOrder = MediaStore.Audio.Media.DEFAULT_SORT_ORDER;
+
+            ContentResolver resolver = getContentResolver();
+
+            Cursor cursor = resolver.query(uri, searchKey, where, keywords, sortOrder);
+
+            while (cursor != null) {
+                // 获取音乐的路径
+                // 这个参数我们实际上不会用到
+                // 不过在调试程序的时候可以方便我们看到音乐的真实路径
+                // 确定寻找的文件的确就在我们规定的目录当中
+                String path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+                // 获取音乐的ID
+                String id = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID));
+                // 通过URI和ID，组合出改音乐特有的Uri地址
+                // 每首音乐都有一个全局唯一的URI地址
+                // 操作某首具体的音乐就可以通过这个地址来完成
+                // 而id就是用来获取该音乐的URI地址的
+                // 将id与音频的URI地址组合一下就能得到特定某首音乐的URI地址
+                // 它的形式就像content://media/external/audio/media/12345，
+                Uri musicUri = Uri.withAppendedPath(uri, id);
+                // 获取音乐的名称
+                String name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                // 获取音乐的时长，单位是毫秒
+                long duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION));
+                // 获取该音乐所在专辑的id
+                int albumId = cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM_ID));
+                // 通过AlbumId组合出专辑的Uri地址
+                // like "content://media/external/audio/albumart/4
+                Uri albumUri = ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), albumId);
+
+                MusicItem data = new MusicItem(musicUri, albumUri, name, duration, 0);
+            }
+
+            cursor.close();
+
+            return null;
+        }
+
+    }
+
 }
